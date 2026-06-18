c0ef750c7340c42fa5167bfd04b8b32e79b75e74
AngrySkype <119269334+AngrySkype@users.noreply.github.com>
2026-06-18 07:44:14 -0700

    rewrite

--- main.py
+++ main.py
@@ -1,52 +1,68 @@
-import random
 import dearpygui.dearpygui as dpg
-import pyperclip as pc
+import random
+import pyperclip
 
-current_password = ""
+dpg.create_context()
 
-def random_pass(length):
-    lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    special = ['@', '#', '$', '%', '&', '*']
+random_number = ""
 
-    all = lower + upper + num + special
-    
-    return "".join(random.choices(all, k=length))
+lower = ["a", "b", "c", "d", "e", "f"]
+upper = ["A", "B", "C", "D", "E", "F"]
+number = ["0", "1", "2", "3", "4", "5", "6"]
+special = ["!", "@", "#", "$", "%", "^", "&", "*"]
+
+all = lower + upper + number + special
+
+def random_words(length):
+    chars = []
+
+    if dpg.get_value("lower"):
+        chars += lower
+
+    if dpg.get_value("upper"):
+        chars += upper
+
+    if dpg.get_value("number"):
+        chars += number
+
+    if dpg.get_value("special"):
+        chars += special
+
+    if not chars:
+        return
+
+    return "".join(random.choice(chars) for _ in range(length))
+
+def slider_callback(sender, app_data):
+    global random_number
+    random_number = random_words(app_data)
+    dpg.set_value("result", value=random_number)
+
+def input_callback(sender, app_data):
+    global random_number
 
 def button_callback(sender, app_data):
-    global current_password
-    length = dpg.get_value("length_slider")
-    x = dpg.get_value("result_input")
-    pc.copy(x)
+    x = dpg.get_value("result")
+    pyperclip.copy(x)
 
-def on_slider_change(sender, app_data):
-    global current_password
-    password = random_pass(app_data)
-    current_password = password
-    dpg.set_value("result_input", current_password)
-
-def result(sender,app_data):
-    pass
-    
 def main():
-    dpg.create_context()
-
     with dpg.window(tag="f"):
-        dpg.add_text("random password generator")
-        dpg.add_slider_int(label="words",default_value=1,max_value=128, callback=on_slider_change,tag="length_slider")
-        dpg.add_input_text(label="result",callback=result,tag="result_input")        
+        dpg.add_text("Simple password generator")
+        dpg.add_slider_int(label="Words",min_value=1,max_value=128, callback=slider_callback)
+        dpg.add_input_text(label="Result", callback=input_callback,tag="result")
         dpg.add_button(label="Copy", callback=button_callback)
 
-    dpg.create_viewport(title='Custom Title', width=600, height=200)
-    dpg.set_primary_window("f",True)
+        dpg.add_checkbox(label="Lowercase", tag="lower")
+        dpg.add_checkbox(label="Uppercase", tag="upper")
+        dpg.add_checkbox(label="Numbers", tag="number")
+        dpg.add_checkbox(label="Special", tag="special")
+
+    dpg.create_viewport(title='Simple password generator', width=600, height=200)
     dpg.setup_dearpygui()
     dpg.show_viewport()
+    dpg.set_primary_window("f", True)
     dpg.start_dearpygui()
     dpg.destroy_context()
 
 if __name__ == "__main__":
     main()
-
-
-
