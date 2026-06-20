1d1deb923632514b5053a93b70ab0e3222459c9d
AngrySkype <119269334+AngrySkype@users.noreply.github.com>
2026-06-20 14:05:20 +0200

    Add password strength presets and random button

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ dpg.create_context()
 
 random_number = ""
 
+preset = ["Normal", "Strong", "Very Strong"]
+password_strength = ["Weak","Strong","Very Strong",""]
+
+
 lower = ["a", "b", "c", "d", "e", "f"]
 upper = ["A", "B", "C", "D", "E", "F"]
 number = ["0", "1", "2", "3", "4", "5", "6"]
@@ -45,17 +49,34 @@ def button_callback(sender, app_data):
     x = dpg.get_value("result")
     pyperclip.copy(x)
 
+def random_callback(sender, app_data):
+    length = dpg.get_value("slider")
+    dpg.set_value("result", random_words(length))
+
+def preset_callback(sender, app_data):
+    dpg.set_value("lower", app_data in ["Normal", "Strong", "Very Strong"])
+    dpg.set_value("upper", app_data in ["Normal", "Strong", "Very Strong"])
+    dpg.set_value("number", app_data in ["Strong", "Very Strong"])
+    dpg.set_value("special", app_data in ["Very Strong"])
+    random_callback(sender,app_data)
+
+
 def main():
     with dpg.window(tag="f"):
         dpg.add_text("Simple password generator")
-        dpg.add_slider_int(label="Words",min_value=1,max_value=128, callback=slider_callback)
+        dpg.add_slider_int(label="Words",min_value=1,max_value=128, callback=slider_callback, tag="slider")
         dpg.add_input_text(label="Result", callback=input_callback,tag="result")
-        dpg.add_button(label="Copy", callback=button_callback)
 
-        dpg.add_checkbox(label="Lowercase", tag="lower")
-        dpg.add_checkbox(label="Uppercase", tag="upper")
-        dpg.add_checkbox(label="Numbers", tag="number")
-        dpg.add_checkbox(label="Special", tag="special")
+        with dpg.group(horizontal=True):
+            dpg.add_button(label="Copy", callback=button_callback)
+            dpg.add_button(label="Random", callback=random_callback)
+            dpg.add_combo(label="Select preset", items=preset, callback=preset_callback,tag="preset")
+
+        with dpg.group(horizontal=True):
+            dpg.add_checkbox(label="Lowercase", tag="lower")
+            dpg.add_checkbox(label="Uppercase", tag="upper")
+            dpg.add_checkbox(label="Numbers", tag="number")
+            dpg.add_checkbox(label="Special", tag="special")
 
     dpg.create_viewport(title='Simple password generator', width=600, height=200)
     dpg.setup_dearpygui()
@@ -63,6 +84,7 @@ def main():
     dpg.set_primary_window("f", True)
     dpg.start_dearpygui()
     dpg.destroy_context()
+
 
 if __name__ == "__main__":
     main()
