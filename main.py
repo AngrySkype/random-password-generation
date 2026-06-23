e65bec55aa797de857aacfb30b1cba3319ff007b
AngrySkype <119269334+AngrySkype@users.noreply.github.com>
2026-06-23 08:56:50 +0200

    Refactor character sets to use string module for improved readability

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import dearpygui.dearpygui as dpg
 import pyperclip
 import secrets
-
+import string
 dpg.create_context()
 
 
 preset = ["Normal", "Strong", "Very Strong"]
 
-lower = ["a", "b", "c", "d", "e", "f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-upper = ["A", "B", "C", "D", "E", "F","G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "'", '"', ",", ".", "<", ">", "/", "?"]
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+number = string.digits
+special = string.punctuation
 
 all_chars = lower + upper + number + special
 
